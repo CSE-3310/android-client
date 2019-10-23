da044ca4babdb2f3e44235e5ed0d09f7290a0b0a
vardaro <anthonyavardaro@yahoo.com>
2019-10-23 19:04:33 -0400

    mock ui with new lib

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,47 +1,30 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import {
-  Image,
   Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  Button,
-  TouchableOpacity,
-  TextInput,
-  View,
+   StyleSheet,
+  View
 } from 'react-native';
 
-import { Container, Button, Text } from 'native-base';
-
-
-import { MonoText } from '../components/StyledText';
+import { Container, Item, Input, Header, Body, Content, Title, Button, Text } from 'native-base';
 
 export default function HomeScreen() {
   const [value, onChangeText] = React.useState('');
 
   return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        
-        <View style={styles.welcomeContainer}>
-          <Text style={{fontSize: 32}}>iResume</Text>
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-            onChangeText={text => onChangeText(text)}
-            value={value}
-          />
-        </View>
-
-
-      </ScrollView>
-
-    </View>
+    <Container>
+    <Header>
+      <Body>
+        <Title>iResume</Title>
+      </Body>
+    </Header>
+    <Content padder style={styles.form}>
+      <Input placeholder='Link resume' />
+      <Button block primary>
+        <Text>Search</Text>
+      </Button>
+    </Content>
+  </Container>
   );
 }
 
@@ -64,77 +47,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 100,
-    marginBottom: 20,
-  },
-  getStartedContainer: {
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-  buttonStyle: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    backgroundColor: '#DDDDDD',
-    padding: 5,
-  },
+  form: {
+    padding: 15,
+  }
 });
